eccd783ffb214158ff5e5e1d07b33ca67d8f6900
rluizaduarte <riellyduarte75@gmail.com>
2025-05-11 15:55:13 -0300

    ProdutoSimples

---
+++ operadores_aritmeticos/ProdutoSimples.cpp
@@ -0,0 +1,20 @@
+/*
+Questão Produto Simples - Rielly Luiza
+https://judge.beecrowd.com/pt/problems/view/1004
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b; 
+    cin >> a;
+    cin >> b;
+    
+    // cout << "PROD = " << a*b << endl;
+    int PROD = a*b;
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
